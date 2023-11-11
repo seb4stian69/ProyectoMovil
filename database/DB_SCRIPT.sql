5f487127360c90b4b91a930a7f2d7a48ada2179c
Sebastian Santis <sebastiandasanhe.8712@gmail.com>
2023-11-11 11:11:30 -0500

    Modificando archivos DB

--- database/DB_SCRIPT.sql
+++ database/DB_SCRIPT.sql
@@ -18,8 +18,8 @@ USE `Artesanias` ;
 -- Table `Artesanias`.`Usuarios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Artesanias`.`Usuarios` (
-  `_id` VARCHAR(15) CHARACTER SET 'big5' NOT NULL UNIQUE ,
-  `tipo_id` ENUM('CC', 'CE', 'TI', 'PAS') NOT NULL UNIQUE ,
+  `_id` VARCHAR(15) CHARACTER SET 'big5' NOT NULL,
+  `tipo_id` ENUM('CC', 'CE', 'TI', 'PAS') NOT NULL,
   `primer_nombre` VARCHAR(45) NOT NULL,
   `segundo_nombre` VARCHAR(45) NULL,
   `primer_apellido` VARCHAR(45) NOT NULL,
@@ -28,14 +28,15 @@ CREATE TABLE IF NOT EXISTS `Artesanias`.`Usuarios` (
   `telefono` VARCHAR(45) NOT NULL,
   `direccion` VARCHAR(45) NOT NULL,
   `correo` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`_id`, `tipo_id`)
-);
+  PRIMARY KEY (`_id`, `tipo_id`))
+;
+
 
 -- -----------------------------------------------------
 -- Table `Artesanias`.`Roles`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Artesanias`.`Roles` (
-  `_id` VARCHAR(100) NOT NULL UNIQUE ,
+  `_id` VARCHAR(100) NULL,
   `tipo` ENUM('Cliente', 'Artesano', 'Admin') NOT NULL,
   `Usuarios__id` VARCHAR(15) CHARACTER SET 'big5' NOT NULL,
   `Usuarios_tipo_id` ENUM('CC', 'CE', 'TI', 'PAS') NOT NULL,
@@ -45,8 +46,9 @@ CREATE TABLE IF NOT EXISTS `Artesanias`.`Roles` (
     FOREIGN KEY (`Usuarios__id` , `Usuarios_tipo_id`)
     REFERENCES `Artesanias`.`Usuarios` (`_id` , `tipo_id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
+    ON UPDATE NO ACTION)
+;
+
 
 -- -----------------------------------------------------
 -- Table `Artesanias`.`Categorias`
@@ -55,15 +57,15 @@ CREATE TABLE IF NOT EXISTS `Artesanias`.`Categorias` (
   `_id` VARCHAR(100) NOT NULL,
   `nombre_categoria` VARCHAR(45) NOT NULL,
   `descripcion` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`_id`)
-);
+  PRIMARY KEY (`_id`))
+;
+
 
 -- -----------------------------------------------------
 -- Table `Artesanias`.`Productos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Artesanias`.`Productos` (
   `_id` VARCHAR(100) NOT NULL,
-  `precio` FLOAT NOT NULL,
   `nombre` VARCHAR(45) NOT NULL,
   `descripcion` VARCHAR(100) NOT NULL,
   `stock` INT NOT NULL,
@@ -77,8 +79,9 @@ CREATE TABLE IF NOT EXISTS `Artesanias`.`Productos` (
     FOREIGN KEY (`Categorias__id`)
     REFERENCES `Artesanias`.`Categorias` (`_id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
+    ON UPDATE NO ACTION)
+;
+
 
 -- -----------------------------------------------------
 -- Table `Artesanias`.`MetodoPagoCompra`
@@ -87,8 +90,9 @@ CREATE TABLE IF NOT EXISTS `Artesanias`.`MetodoPagoCompra` (
   `_id` VARCHAR(100) NOT NULL,
   `medio` ENUM('TDebito', 'TCredito', 'Efectivo', 'Transferencia') NOT NULL,
   `detalles` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`_id`)
-);
+  PRIMARY KEY (`_id`))
+;
+
 
 -- -----------------------------------------------------
 -- Table `Artesanias`.`Facturas`
@@ -97,23 +101,24 @@ CREATE TABLE IF NOT EXISTS `Artesanias`.`Facturas` (
   `_id` VARCHAR(100) NOT NULL,
   `fecha` DATE NOT NULL,
   `estado` ENUM('EnProceso', 'Cancelado', 'Entregado') NOT NULL,
-  `MetodoPagoCompra__id` VARCHAR(100) NOT NULL,
   `Usuarios__id` VARCHAR(15) CHARACTER SET 'big5' NOT NULL,
   `Usuarios_tipo_id` ENUM('CC', 'CE', 'TI', 'PAS') NOT NULL,
+  `MetodoPagoCompra__id` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`_id`),
-  INDEX `fk_Facturas_MetodoPagoCompra1_idx` (`MetodoPagoCompra__id` ASC) VISIBLE,
   INDEX `fk_Facturas_Usuarios1_idx` (`Usuarios__id` ASC, `Usuarios_tipo_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Facturas_MetodoPagoCompra1`
-    FOREIGN KEY (`MetodoPagoCompra__id`)
-    REFERENCES `Artesanias`.`MetodoPagoCompra` (`_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  INDEX `fk_Facturas_MetodoPagoCompra1_idx` (`MetodoPagoCompra__id` ASC) VISIBLE,
   CONSTRAINT `fk_Facturas_Usuarios1`
     FOREIGN KEY (`Usuarios__id` , `Usuarios_tipo_id`)
     REFERENCES `Artesanias`.`Usuarios` (`_id` , `tipo_id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Facturas_MetodoPagoCompra1`
+    FOREIGN KEY (`MetodoPagoCompra__id`)
+    REFERENCES `Artesanias`.`MetodoPagoCompra` (`_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
 
 -- -----------------------------------------------------
 -- Table `Artesanias`.`DetallesFactura`
@@ -126,19 +131,20 @@ CREATE TABLE IF NOT EXISTS `Artesanias`.`DetallesFactura` (
   `Facturas__id` VARCHAR(100) NOT NULL,
   `Productos__id` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`_id`),
-  INDEX `fk_Detalles_Facturas1_idx` (`Facturas__id` ASC) VISIBLE,
-  INDEX `fk_Detalles_Productos1_idx` (`Productos__id` ASC) VISIBLE,
-  CONSTRAINT `fk_Detalles_Facturas1`
+  INDEX `fk_DetallesFactura_Facturas1_idx` (`Facturas__id` ASC) VISIBLE,
+  INDEX `fk_DetallesFactura_Productos1_idx` (`Productos__id` ASC) VISIBLE,
+  CONSTRAINT `fk_DetallesFactura_Facturas1`
     FOREIGN KEY (`Facturas__id`)
     REFERENCES `Artesanias`.`Facturas` (`_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Detalles_Productos1`
+  CONSTRAINT `fk_DetallesFactura_Productos1`
     FOREIGN KEY (`Productos__id`)
     REFERENCES `Artesanias`.`Productos` (`_id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
+    ON UPDATE NO ACTION)
+;
+
 
 -- -----------------------------------------------------
 -- Table `Artesanias`.`Credenciales`
@@ -155,8 +161,9 @@ CREATE TABLE IF NOT EXISTS `Artesanias`.`Credenciales` (
     FOREIGN KEY (`Usuarios__id` , `Usuarios_tipo_id`)
     REFERENCES `Artesanias`.`Usuarios` (`_id` , `tipo_id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
+    ON UPDATE NO ACTION)
+;
+
 
 -- -----------------------------------------------------
 -- Table `Artesanias`.`VideosArtesanos`
@@ -175,8 +182,9 @@ CREATE TABLE IF NOT EXISTS `Artesanias`.`VideosArtesanos` (
     FOREIGN KEY (`Usuarios__id` , `Usuarios_tipo_id`)
     REFERENCES `Artesanias`.`Usuarios` (`_id` , `tipo_id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
+    ON UPDATE NO ACTION)
+;
+
 
 -- -----------------------------------------------------
 -- Table `Artesanias`.`IngresosLogs`
@@ -193,5 +201,10 @@ CREATE TABLE IF NOT EXISTS `Artesanias`.`IngresosLogs` (
     FOREIGN KEY (`Usuarios__id` , `Usuarios_tipo_id`)
     REFERENCES `Artesanias`.`Usuarios` (`_id` , `tipo_id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
+    ON UPDATE NO ACTION)
+;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
