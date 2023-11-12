5ca8b4e980807e06297e49fb35dccf642ca7305f
Sebastian Santis <sebastiandasanhe.8712@gmail.com>
2023-11-11 21:50:43 -0500

    Modificando DB

--- database/DB_SCRIPT.sql
+++ database/DB_SCRIPT.sql
@@ -73,11 +73,19 @@ CREATE TABLE IF NOT EXISTS `Artesanias`.`Productos` (
   `valor_venta` FLOAT NOT NULL,
   `imagen` VARCHAR(45) NOT NULL,
   `Categorias__id` VARCHAR(100) NOT NULL,
+  `Usuarios__id` VARCHAR(15) CHARACTER SET 'big5' NOT NULL,
+  `Usuarios_tipo_id` ENUM('CC', 'CE', 'TI', 'PAS') NOT NULL,
   PRIMARY KEY (`_id`),
   INDEX `fk_Productos_Categorias1_idx` (`Categorias__id` ASC) VISIBLE,
+  INDEX `fk_Productos_Usuarios1_idx` (`Usuarios__id` ASC, `Usuarios_tipo_id` ASC) VISIBLE,
   CONSTRAINT `fk_Productos_Categorias1`
     FOREIGN KEY (`Categorias__id`)
     REFERENCES `Artesanias`.`Categorias` (`_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Productos_Usuarios1`
+    FOREIGN KEY (`Usuarios__id` , `Usuarios_tipo_id`)
+    REFERENCES `Artesanias`.`Usuarios` (`_id` , `tipo_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ;
@@ -204,11 +212,12 @@ CREATE TABLE IF NOT EXISTS `Artesanias`.`IngresosLogs` (
     ON UPDATE NO ACTION)
 ;
 
+
 -- -----------------------------------------------------
 -- Table `Artesanias`.`CarritoCompras`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Artesanias`.`CarritoCompras` (
-  `_id` INT NOT NULL AUTO_INCREMENT,
+  `_id` INT NOT NULL,
   `Usuarios__id` VARCHAR(15) CHARACTER SET 'big5' NOT NULL,
   `Usuarios_tipo_id` ENUM('CC', 'CE', 'TI', 'PAS') NOT NULL,
   `Productos__id` VARCHAR(100) NOT NULL,
@@ -227,6 +236,7 @@ CREATE TABLE IF NOT EXISTS `Artesanias`.`CarritoCompras` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
